6dd0512eae1f890295101c1468ec6c1d6a4f153e
zhongyuze <zhongyuze@mskj.com>
2023-01-08 22:56:19 +0800

    testo01

---
+++ src/main/java/com/example/testgit/TestController.java
@@ -0,0 +1,12 @@
+package com.example.testgit;
+
+/**
+ * @author: zhongyuze
+ * @date: 2023-01-08 22:52
+ * @description: TODO
+ * @Version 1.0
+ **/
+public class TestController {
+
+	public static final String code="hhhh";
+}
